a78272b08f26c63f9254db8fed3907a023848270
28ede1 <ede1@williams.edu>
2025-01-25 15:18:21 -0500

    testing menus

--- code/game.py
+++ code/game.py
@@ -261,8 +261,8 @@ class Menu():
         close_window()
 
 if __name__ == '__main__':
-    #menu_test = Menu()
-    #menu_test.run_menu()
-    game_test = SpaceGame()
-    game_test.run()
+    menu_test = Menu()
+    menu_test.run_menu()
+    #game_test = SpaceGame()
+    #game_test.run()
     
