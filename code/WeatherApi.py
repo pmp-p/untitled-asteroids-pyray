f597ddbccd26624c637e479e68bb9c402ac98f16
28ede1 <ede1@williams.edu>
2025-04-09 08:42:08 -0400

    Added WeatherApi.py

--- code/WeatherApi.py
+++ code/WeatherApi.py
@@ -1,6 +1,4 @@
 import requests
-from dotenv import load_dotenv
-import os
 
 """
 This API parses data from a weather information database. This implementation specifically retreives 
@@ -21,7 +19,6 @@ note2:
 """
 
 # Load environment variables from .env file, be sure so replace .env variables with your API information
-load_dotenv() 
 
 def get_city_temp_wspd(city):
     """
@@ -29,11 +26,11 @@ def get_city_temp_wspd(city):
     Converts temperature from Kelvin to Fahrenheit and returns wind speed.
     """
 
-    url = os.getenv("DATABASE_URL")
+    url = None
 
     headers = {
-        "x-rapidapi-key": os.getenv("SECRET_KEY"),
-        "x-rapidapi-host": os.getenv("HOST"),
+        "x-rapidapi-key": None,
+        "x-rapidapi-host": None,
     }
 
     # Use the city name in the querystring to fetch relevant weather data
